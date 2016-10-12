2f5cb15a76a45d77726d8fea3cf691fbb1daf148
deliayangjobs <deliayangjobs@gmail.com>
2016-10-12 13:57:24 -0700

    Created a Map Shows Volcanoes and World Population
    Need install:
    
    - Python
    - Folium
    - Pandas

---
+++ mapProject.py
@@ -0,0 +1,32 @@
+import folium
+import pandas
+
+
+df=pandas.read_csv('Volcanoes-USA.txt')
+map=folium.Map(location=[df['LAT'].mean(), df['LON'].mean()], zoom_start=6, tiles='Mapbox Bright')
+
+def color(elev):
+    minimun=int(min(df['ELEV']))
+    step=int((max(df['ELEV'])-min(df['ELEV']))/3)
+    if elev in range(minimun, minimun+step):
+        col='green'
+    elif elev in range(minimun+step, minimun+step+step):
+        col='orange'
+    else:
+        col='red'
+    return col
+
+fg=folium.FeatureGroup(name='Volcano Locations')
+for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
+    #map.simple_marker(location=[lat, lon], popup=name, marker_color='green' if elev in range(0,1000) else 'orange' if elev in range(1000, 3000) else 'red')
+    fg.add_child(folium.Marker(location=[lat, lon], popup=name, icon=folium.Icon(color=color(elev), icon_color='gray')))
+map.add_child(fg)
+
+map.add_child(folium.GeoJson(data=open('World_population.json'),
+name='World Population',
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']<=10000000 else 'orange' if 10000000<x['properties']['POP2005']<20000000 else 'red'}
+))
+
+map.add_child(folium.LayerControl())
+
+map.save(outfile='test.html')
